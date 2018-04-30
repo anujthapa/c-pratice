6143fa56c5759adaf2901dbc76ca72407f4f46e4
anujthapa <33247998+anujthapa@users.noreply.github.com>
2018-04-30 17:34:28 +0300

    Add files via upload

---
+++ loopsansstring/textchanger/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace textchanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence;
+            string firstChar;
+            string restOfWord;
+            string pigWord;
+            string vowels = "AEIOUaeiou";
+            int letterpos;
+            Console.WriteLine("Enter sentence you want to change in pigtext!");
+            sentence = Console.ReadLine();
+            foreach (string word in sentence.Split())
+            {
+                firstChar = word.Substring(0, 1);
+                restOfWord = word.Substring(1, word.Length - 1);
+                letterpos = vowels.IndexOf(firstChar);
+                if(letterpos== -1)
+                {
+                    pigWord = restOfWord + firstChar + "ay";
+                    
+                }
+                else
+                {
+                    pigWord = word + "way";
+                }
+                Console.Write(pigWord);
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
